76d5227e506e4ed4ccc346439ccc2b34efda6b5a
pranav <dspranav.calps@gmail.com>
2024-02-20 00:26:34 -0600

    BasicNewCommit

--- Backend/Insp.py
+++ Backend/Insp.py
@@ -2,11 +2,27 @@ from pyravendb.store import document_store
 
 # Set up RavenDB document store
 class USERS:
-     
      def __init__(self,username,password,address) -> None:
           self.username=username
           self.password=password
           self.address=address
+
+class Items:
+      def __init__(self,Itemid,productname,productprice) -> None:
+            self.Itemid=Itemid
+            self.productname=productname
+            self.productprice=productprice
+class Reviews:
+      def __init__(self,ReviewId,Username,Description) -> None:
+            self.ReviewId=ReviewId
+            self.username=Username
+            self.Description=Description
+class Shares:
+      def __init__(self,username,Itemshared,itemreference) -> None:
+             self.username=username
+             self.Itemshared=Itemshared
+             self.itemreference=itemreference 
+
 store = document_store.DocumentStore(urls=["https://a.exchangemart.ravendb.community:8080"], database="Exchangemart")
 store.initialize()
 session = store.open_session()
